15608dea9256f3a42c8bcfc8dcb0364874d1db48
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2026-05-15 15:03:21 +0200

    Cleanup

--- internal/docker/clean.go
+++ internal/docker/clean.go
@@ -4,52 +4,22 @@ import "strings"
 
 // CleanImage removes all containers using image and the image itself.
 func CleanImage(image string) error {
-	args := []string{
-		"ps",
-		arg("all"),
-		arg("quiet"),
-		arg("filter", "label=project=agentic-cli"),
-		arg("filter", "ancestor="+image),
-	}
-	out, err := dockerRun(args...)
-	if err != nil {
+	if err := runIfAny(
+		[]string{"ps", arg("all"), arg("quiet"), arg("filter", "label=project=agentic-cli"), arg("filter", "ancestor="+image)},
+		[]string{"rm", arg("force")},
+	); err != nil {
 		return err
 	}
 
-	if ids := strings.Fields(out); len(ids) > 0 {
-		args = append([]string{"rm", arg("force")}, ids...)
-		if _, err := dockerRun(args...); err != nil {
-			return err
-		}
-	}
-
-	args = []string{
-		"images",
-		arg("quiet"),
-		image,
-	}
-	out, err = dockerRun(args...)
-	if err != nil {
-		return err
-	}
-
-	if ids := strings.Fields(out); len(ids) > 0 {
-		args = append([]string{"rmi", arg("force")}, ids...)
-		if _, err := dockerRun(args...); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return runIfAny(
+		[]string{"images", arg("quiet"), image},
+		[]string{"rmi", arg("force")},
+	)
 }
 
 // CleanBaseImages removes all Docker images whose repository starts with "agentic-base".
 func CleanBaseImages() error {
-	args := []string{
-		"images",
-		arg("format", "{{.Repository}}"),
-	}
-	out, err := dockerRun(args...)
+	out, err := dockerRun("images", arg("format", "{{.Repository}}"))
 	if err != nil {
 		return err
 	}
@@ -61,12 +31,21 @@ func CleanBaseImages() error {
 		}
 	}
 
-	if len(names) > 0 {
-		args = append([]string{"rmi", arg("force")}, names...)
-		if _, err := dockerRun(args...); err != nil {
-			return err
-		}
+	if len(names) == 0 {
+		return nil
 	}
+	_, err = dockerRun(append([]string{"rmi", arg("force")}, names...)...)
+	return err
+}
 
+func runIfAny(listArgs, runArgs []string) error {
+	out, err := dockerRun(listArgs...)
+	if err != nil {
+		return err
+	}
+	if ids := strings.Fields(out); len(ids) > 0 {
+		_, err = dockerRun(append(runArgs, ids...)...)
+		return err
+	}
 	return nil
 }
